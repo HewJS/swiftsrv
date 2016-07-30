ab13833a8ccb07f78cae31b629cc43f75495b99b
Peranat Dayananda <peranatdayananda@gmail.com>
2016-07-30 10:06:29 +0700

    (feat) Add schema for data

---
+++ database/model/dataModel.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+// Dataset will be a collection of data from the front end
+// based on user input and the destination eventually `picked'
+// Sample: {
+//    emoticons: { smiley: 1, wink: 1, crying: 0, sad: 0 },
+//    selected: { type: 'mexican', keyword: 'tacos' }
+// }
+// This can and will change
+
+const dataSchema = new mongoose.Schema({
+  emoticons: Object,
+  selected: Object
+});
+
+export { dataSchema };
